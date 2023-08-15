34b4c2029638ed204822584d81263cb825577fa3
prasanshivarapu <prasan.shivarapu@gmail.com>
2023-08-15 17:08:19 +0530

    done

---
+++ src/components/file2/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+
+const Types = () => (
+  <div className="type">
+    <button className="but1" type="button ">
+      Overview
+    </button>
+    <button className="but1" type="button ">
+      Benfit
+    </button>
+    <button className="but1" type="button ">
+      Types
+    </button>
+    <button className="but1" type="button ">
+      Listicles
+    </button>
+    <button className="but1" type="button ">
+      Registration Procedure
+    </button>
+    <button className="but1" type="button ">
+      Other Information
+    </button>
+    <button className="but1" type="button ">
+      Corpbiz Support
+    </button>
+    <button className="but1" type="button ">
+      Faq
+    </button>
+  </div>
+)
+
+export default Types
